555127f584e2441d071461002d975bdffc2fce07
Ali_Shair <alishair7071@gmail.com>
2025-03-01 23:04:11 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './routes/App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 import CreatePost from './components/CreatePost.jsx';
 import PostList from './components/PostList.jsx';
 
-const router = createBrowserRouter(
+const router = createHashRouteruter(
   [
     { path: "/",
        element: <App/>,
